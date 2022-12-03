f5929ddbb38348613a65bbc932277a2b434fb26e
Cammy Mun <cammy.mun@gmail.com>
2022-12-03 14:18:52 +0800

    schedule new

--- techtrek-frontend/src/views/transactionspage/Transactions.js
+++ techtrek-frontend/src/views/transactionspage/Transactions.js
@@ -1,8 +1,39 @@
 import React from 'react'
 import JsonData from './ScheduledTransactions.json'
-import { TbTrash } from "react-icons/tb";
+import { TbTrash, TbPlaylistAdd } from "react-icons/tb";
+import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import SendIcon from '@mui/icons-material/Send';
+
+const style = {
+  // position: 'absolute' as 'absolute',
+  // top: '50%',
+  // left: '50%',
+  // transform: 'translate(-50%, -50%)',
+  width: 800,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
  function JsonDataDisplay(){
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [value, setValue] = React.useState(dayjs('2022-04-07'));
+
     const DisplayData=JsonData.map(
         (info)=>{
             return(
@@ -34,13 +65,65 @@ import { TbTrash } from "react-icons/tb";
                     </tr>
                 </thead>
                 <tbody>
-                 
-                    
                     {DisplayData}
                     
                 </tbody>
             </table>
-             
+          <div onClick={() => console.log('yes')}><TbPlaylistAdd/></div>    
+          <Button onClick={handleOpen}>Open modal</Button>
+
+          
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        // BackdropComponent={Backdrop}
+        // BackdropProps={{
+        //   timeout: 500,
+        // }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Schedule new Transaction
+            </Typography>
+            {/* <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography> */}
+            <TextField
+          required
+          id="outlined-required"
+          label="Bank Account ID"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="Transaction Amount"
+          defaultValue=""
+        />
+        <TextField
+          required
+          id="standard-search"
+          label="Comment"
+          defaultValue=""
+        />
+        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DateTimePicker
+        renderInput={(props) => <TextField {...props} />}
+        label="DateTimePicker"
+        value={value}
+        onChange={(newValue) => {
+          setValue(newValue);
+        }}
+      />
+    </LocalizationProvider> */}
+        <Button onClick={handleOpen}>Submit</Button>
+          </Box>
+        </Fade>
+      </Modal>
         </div>
     )
  }
