408cdd4ca9d9c9bca065ea73ccb1d0bdc9f5d55e
nghaoen <nghaoen@gmail.com>
2022-12-03 14:22:35 +0800

    added axios

--- techtrek-frontend/src/views/bank-accounts/BankAccount.js
+++ techtrek-frontend/src/views/bank-accounts/BankAccount.js
@@ -1,24 +1,18 @@
 import './BankAccount.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Account } from '../../components/Account';
 import NavBar from '../../components/NavBar';
+import axios from 'axios';
 
 export const BankAccount = () => {
     const AccountID = 621156213;
-    const [accountData,setAccountData] = useState([
-        {
-            "AccountID": 621156213,
-            "UserID": 1,
-            "AccountType": "Saving",
-            "AccountBalance": 70200.71
-        },
-        {
-            "AccountID": 958945214,
-            "UserID": 1,
-            "AccountType": "Current",
-            "AccountBalance": 99720.46
-        }
-    ])
+    const [accountData,setAccountData] = useState([])
+    useEffect(() => {
+        axios.get(`https://flask-production-7a20.up.railway.app/account/4`)
+        .then((res) => setAccountData(res.data.data.bank_account))
+      });
+    
+
   return (
     <div className="Bank-Account">
         <NavBar />
