b32835e2cbd2fcd792f80ee2790712ddb1d46bc5
nghaoen <nghaoen@gmail.com>
2022-12-03 11:44:50 +0800

    bank account first commit

--- techtrek-frontend/src/views/bank-accounts/BankAccount.js
+++ techtrek-frontend/src/views/bank-accounts/BankAccount.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { useState } from 'react';
+import NavBar from '../../components/NavBar';
 
 export const BankAccount = () => {
+    const AccountID = 621156213;
+    const AccountData = [
+        {
+            "AccountID": 621156213,
+            "UserID": 1,
+            "AccountType": "Saving",
+            "AcccountBalance": 70200.71
+        },
+        {
+            "AccountID": 958945214,
+            "UserID": 1,
+            "AccountType": "Current",
+            "AcccountBalance": 99720.46
+        }
+    ];
   return (
     <div>
+        <NavBar />
         <h1>
             This is the bank account page.
         </h1>
