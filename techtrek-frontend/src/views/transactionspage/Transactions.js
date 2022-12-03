6147e9e2f2c9246ef4f5da25b2af756268ff411e
Cammy Mun <cammy.mun@gmail.com>
2022-12-03 15:53:02 +0800

    delete

--- techtrek-frontend/src/views/transactionspage/Transactions.js
+++ techtrek-frontend/src/views/transactionspage/Transactions.js
@@ -1,59 +1,47 @@
-import React from 'react'
-import JsonData from './ScheduledTransactions.json'
+import React, {useEffect} from 'react'
+// import JsonData from './ScheduledTransactions.json'
 import { TbTrash, TbPlaylistAdd } from "react-icons/tb";
-import Backdrop from '@mui/material/Backdrop';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Fade from '@mui/material/Fade';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
-import dayjs from 'dayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import SendIcon from '@mui/icons-material/Send';
-
-const style = {
-  // position: 'absolute' as 'absolute',
-  // top: '50%',
-  // left: '50%',
-  // transform: 'translate(-50%, -50%)',
-  width: 800,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-};
+import axios from 'axios';
 
  function JsonDataDisplay(){
 
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [arr, setArr] = React.useState([]);
 
-  const [value, setValue] = React.useState(dayjs('2022-04-07'));
-
-    const DisplayData=JsonData.map(
-        (info)=>{
-            return(
-                <tr>
-                    <td>{info.TransactionID}</td>
-                    <td>{info.AccountID}</td>
-                    <td>{info.ReceivingAccountID}</td>
-                    <td>{info.Date}</td>
-                    <td>{info.TransactionAmount}</td>
-                    <td>{info.Comment}</td>
-                    <td onClick={() => console.log(info.TransactionID)}><TbTrash /></td>
-                </tr>
-            )
-        }
+  useEffect(()=>{
+    axios.get('https://flask-production-7a20.up.railway.app/transactions/621156213')
+    .then(
+      (res) => {
+        const arrayCop = [...res.data.data.transaction_lists]
+        setArr(arrayCop)
+      }
     )
- 
+
+  }, [])
+
+  const remove = (TransactionID) => {
+    const arrayCopy = arr.filter((row) => row.TransactionID !== TransactionID);
+    setArr(arrayCopy)
+  };
+
+const table_result = arr.map((item)=> {
+  return (
+    <tr>
+    <td>{item.TransactionID}</td>
+    <td>{item.AccountID}</td>
+    <td>{item.ReceivingAccountID}</td>
+    <td>{item.Date}</td>
+    <td>{item.TransactionAmount}</td>
+    <td>{item.Comment}</td>
+    <td onClick={() => remove(item.TransactionID)}><TbTrash /></td>
+</tr>
+  )
+})
+
     return(
+      
         <div>
-            <table class="table table-striped">
-                <thead>
+          <table class="table table-striped">
+          <thead>
                     <tr>
                       <th>Transaction ID</th>
                       <th>Account ID</th>
@@ -64,174 +52,10 @@ const style = {
                       <th>Delete</th>
                     </tr>
                 </thead>
-                <tbody>
-                    {DisplayData}
-                    
-                </tbody>
-            </table>
-          <div onClick={() => console.log('yes')}><TbPlaylistAdd/></div>    
-          <Button onClick={handleOpen}>Open modal</Button>
-
-          
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        // BackdropComponent={Backdrop}
-        // BackdropProps={{
-        //   timeout: 500,
-        // }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Schedule new Transaction
-            </Typography>
-            {/* <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-            </Typography> */}
-            <TextField
-          required
-          id="outlined-required"
-          label="Bank Account ID"
-          defaultValue=""
-        />
-        <TextField
-          required
-          id="outlined-required"
-          label="Transaction Amount"
-          defaultValue=""
-        />
-        <TextField
-          required
-          id="standard-search"
-          label="Comment"
-          defaultValue=""
-        />
-        {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DateTimePicker
-        renderInput={(props) => <TextField {...props} />}
-        label="DateTimePicker"
-        value={value}
-        onChange={(newValue) => {
-          setValue(newValue);
-        }}
-      />
-    </LocalizationProvider> */}
-        <Button onClick={handleOpen}>Submit</Button>
-          </Box>
-        </Fade>
-      </Modal>
+          {table_result}
+      </table>
         </div>
     )
  }
 
  export default JsonDataDisplay;
-
-
-// import * as React from 'react';
-// import { DataGrid } from '@mui/x-data-grid';
-
-// const columns = [
-//   { field: 'id', headerName: 'ID', width: 70 },
-//   { field: 'firstName', headerName: 'First name', width: 130 },
-//   { field: 'lastName', headerName: 'Last name', width: 130 },
-//   {
-//     field: 'age',
-//     headerName: 'Age',
-//     type: 'number',
-//     width: 90,
-//   },
-//   {
-//     field: 'fullName',
-//     headerName: 'Full name',
-//     description: 'This column has a value getter and is not sortable.',
-//     sortable: false,
-//     width: 160,
-//     valueGetter: (params) =>
-//       `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-//   },
-// ];
-
-// const rows = [
-//   { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-// ];
-
-// export const DataTable = () => {
-//   return (
-//     <div style={{ height: 400, width: '100%' }}>
-//       <DataGrid
-//         rows={rows}
-//         columns={columns}
-//         pageSize={5}
-//         rowsPerPageOptions={[5]}
-//         checkboxSelection
-//       />
-//     </div>
-//   );
-// }
-
-
-// import * as React from 'react';
-// import { DataGrid } from '@mui/x-data-grid';
-
-// export default function DataTable() {
-//     const data = [
-//         { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-//         { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-//         { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-//         { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-//         { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-//         { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-//         { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-//         { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-//         { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-//     ];
-
-//     return (
-//         <div className="App">
-//          <table>
-//            <tbody>
-    
-             
-//            </tbody>
-//          </table>
-//         </div>
-//       );
-// }
-
-
-
-// export const DataTable = () => {
-//   return (
-//     <div style={{ height: 400, width: '100%' }}>
-//       <DataGrid
-//         rows={rows}
-//         columns={columns}
-//         pageSize={5}
-//         rowsPerPageOptions={[5]}
-//         checkboxSelection
-//       />
-//     </div>
-//   );
-// }
-
-// export const Transactions = () => {
-//     return (
-//       <div>
-//           <h1>
-//               This is the transactions page.
-//           </h1>
-//       </div>
-//     )
-// }
